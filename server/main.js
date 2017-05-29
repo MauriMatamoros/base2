fd345d1f33b3c4271212607edb1f9a7aa26c8ca4
Elmer Lopez <memelopez@Elmers-MacBook-Air.local>
2017-05-29 15:50:24 -0600

    el metodo en el main.js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { Recipes } from '../imports/api/index'
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Meteor.methods({
+    'Recipes.nuevaReceta'( data ) {
+      var nuevaReceta = {
+        ingredientes: data.ingredientes,
+        tipo: data.tipo,
+        tiempo: data.tiempo,
+        precio: data.precio
+      }
+      Recipes.insert( nuevaReceta);
+      return 'ok';
+    }
+  });
 });
